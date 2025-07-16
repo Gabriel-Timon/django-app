13b506193f69b96ae69480e9a0ebdeac8f5cb776
Gabriel Timon <lanzoni.timon@gmail.com>
2025-07-15 21:38:50 -0300

    rev_1

---
+++ galeria/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse(
+        '<h1>Alura Space<h1/>'
+        '<p>Seja muito bem vindo(a)</p>'
+        )
+
